2ac49117ab9dd41dbd5c5b632ba89c7c019b2ed8
Francis John Magallanes <60374264+Francis-Magallanes@users.noreply.github.com>
2021-08-29 22:54:36 +0800

    adding text to image done, and gitignore modif

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
-#change the filepath to where the template for the certificate can be found
-filepath_certificate = "assets/Sci-Py Certificate Template.png"
+#this will handle the processing of one certificate
+#it will return an edited Image object
+def Process_OneName(certificate_img: Image, name:str, position:tuple):
+    
+    certificate_drawer = ImageDraw.Draw(certificate_img)
 
-#change this to your desire point where the name will be placed
-#you can use the test_position.py to determine your desired point
-placement_coordinate = (100, 100) 
+    #load the neccesary font. Change this according to your desired font and size
+    #Note: This font is clear sans bold
+    font = ImageFont.truetype(font = "assets/ClearSans-Bold.ttf", size = 85)
 
+    certificate_drawer.text(position, name, fill = "black", font=font, anchor="ms")
+
+   
+
+    
 def main():
+    
+    #change the filepath to where the template for the certificate can be found
+    filepath_certificate = "assets/Sci-Py Certificate Template.png"
+    certificate_img = Image.open(filepath_certificate)
+
+    width_img, height_img = certificate_img.size
+
+    #change this to your desire point where the name will be placed
+    #you can use the test_position.py to determine your desired point
+    placement_coordinate = (width_img/2, (height_img/2) - 30) 
+
+    Process_OneName(certificate_img, "Francis John N. Magallanes", placement_coordinate)
+
+    certificate_img.show()
+    
+  
+    
     pass
 
 if (__name__ == "__main__"):
--- test_position.py
+++ test_position.py
@@ -12,7 +12,7 @@ certificate = Image.open(filepath_certificate)
 width, height = certificate.size
 
 #change this to your desire point where the name will be placed
-placement_coordinate = (width/2, height/2) 
+placement_coordinate = (width/2, (height/2) - 50) 
 
 certificate_drawer = ImageDraw.Draw(certificate)
 certificate_drawer.point([placement_coordinate], fill = (255,0,0))
